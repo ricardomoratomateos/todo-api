e517f0a1088f90e6b7ec3575ade5697a50d8942b
ricardomoratomateos <ricardomoratomateos@outlook.com>
2020-12-19 15:50:43 +0100

    improve tests docker

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 # Script for run dockers and execute tests inside them
 
-docker-compose -f docker-compose.test.yml up --detach
+docker-compose -p 'todo-api-test' -f docker-compose.test.yml up -d
 
 case "$1" in
     "unit")
-        docker-compose exec -T php ./vendor/bin/phpunit --testsuit unit
+        docker-compose -p 'todo-api-test' exec -T php ./vendor/bin/phpunit --testsuit unit
         ;;
     "integration")
-        docker-compose exec -T php ./vendor/bin/phpunit --testsuit integration
+        docker-compose -p 'todo-api-test' exec -T php ./vendor/bin/phpunit --testsuit integration
         ;;
     *)
-        docker-compose exec -T php ./vendor/bin/phpunit
+        docker-compose -p 'todo-api-test' exec -T php ./vendor/bin/phpunit
         ;;
     esac
 
-docker-compose stop
+docker-compose -p 'todo-api-test' stop
